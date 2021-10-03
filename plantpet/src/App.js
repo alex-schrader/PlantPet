6a1006b6bcd0ffce6b57d8d36a1974d76cb7e74d
Dohyun Lee <leedohyun80@gmail.com>
2021-10-03 16:08:41 -0400

    merge

--- plantpet/src/App.js
+++ plantpet/src/App.js
@@ -5,18 +5,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={plant}/>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Plant Pet
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Exampletext/>
+        <Exampletext/>
       </header>
     </div>
   );
