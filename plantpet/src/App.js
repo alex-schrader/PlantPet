0e2ef9f0c4d06de88cdaff70fd6f73395fe88593
aschrad1 <aschrad1@swarthmore.edu>
2021-10-03 16:00:46 -0400

    adding plant img

--- plantpet/src/App.js
+++ plantpet/src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import Exampletext from './Exampletext.js'
+import plant from './plant.png'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={plant}/>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Plant Pet
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn plantpet
-        </a>
         <Exampletext/>
         <Exampletext/>
       </header>
