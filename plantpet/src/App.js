5e0f1397bef4fe4ecfac948fdfcc8a36654d62d5
aschrad1 <aschrad1@swarthmore.edu>
2021-10-04 15:15:26 -0400

    moving elements

--- plantpet/src/App.js
+++ plantpet/src/App.js
@@ -1,38 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
-import plant from './plant.png'
-import Exampletext from './Exampletext'
-import {useState} from 'react'
-import ProgressBar from 'react-bootstrap/ProgressBar'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Levels from './Components/Levels';
+import logo from "./logo.svg";
+import "./App.css";
+import plant from "./plant.png";
+import Exampletext from "./Exampletext";
+import { useState } from "react";
+import ProgressBar from "react-bootstrap/ProgressBar";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Levels from "./Components/Levels";
 
 function App() {
-  const [water, setWater] = useState(0)
+  const [water, setWater] = useState(0);
   const clickHandlerWater = () => {
-    if (water === 20){
-      setWater(1)
+    if (water === 20) {
+      setWater(1);
+      setLevel(level + 1);
+      console.log('hi')
+      console.log(level)
     } else {
-      setWater(water+1)
+      setWater(water + 1);
     }
-  }
+  };
+  const [level, setLevel] = useState(1);
 
   return (
     <div className="App">
-      <Levels water = {water}/>
-        <img src={plant}/>
-        <p>
-          Plant Pet
-        </p>
-        <Exampletext/>
-        <Exampletext/>
-        <div className = 'progBar'>
-          <ProgressBar animated variant="success" now={water} max={20}/>
-        </div>
-        <button onClick={clickHandlerWater} className = 'waterButton'>
-          WATER
-        </button>
-        <h1>{water}</h1>
+      <Levels level={level} />
+      <img src={plant} />
+      <p>Plant Pet</p>
+      <Exampletext />
+      <Exampletext />
+      <div className="progBar">
+        <ProgressBar animated variant="success" now={water} max={20} />
+      </div>
+      <button onClick={clickHandlerWater} className="waterButton">
+        WATER
+      </button>
+      <h1>{water}</h1>
     </div>
   );
 }
--- plantpet/src/Components/Levels.js
+++ plantpet/src/Components/Levels.js
@@ -1,22 +1,16 @@
-import ProgressBar from 'react-bootstrap/ProgressBar'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import {useState} from 'react'
+import ProgressBar from "react-bootstrap/ProgressBar";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
 
 const Levels = (props) => {
-    const [level, setLevel] = useState(1)
-    const levelHandler = () => {
-      if (props.water === 20) {
-        setLevel(level+1)
-      }
-    }
 
 
-    return (
-        <div className = 'level'>
-         Level: <h1>{level}</h1>
-          <ProgressBar animated variant = "success" now = {level}/>
-        </div>  
-    )
-}
+  return (
+    <div className="level">
+      Level: <h1>{props.level}</h1>
+      <ProgressBar animated variant="success" now={props.level} />
+    </div>
+  );
+};
 
-export default Levels
+export default Levels;
