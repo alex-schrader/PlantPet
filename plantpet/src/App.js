a5e30067aa84d6fd3aa239c2463e8c346bbbc728
aschrad1 <aschrad1@swarthmore.edu>
2021-10-23 21:56:46 -0400

    added basic login functionality with Auth0

--- plantpet/src/App.js
+++ plantpet/src/App.js
@@ -5,16 +5,19 @@ import ProgressBar from "react-bootstrap/ProgressBar";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Levels from "./Components/Levels";
 import "./App.css";
-import waterplant from './waterplant.jpg'
+import waterplant from "./waterplant.jpg";
+import LoginButton from "./Components/Loginout/LoginButton";
+import LogoutButton from "./Components/Loginout/LogoutButton";
+import Profile from "./Components/Profile";
 
 function App() {
   const [water, setWater] = useState(0);
   const clickHandlerWater = () => {
     if (water === 20) {
       setWater(0);
-      setLevel(level + .25);
-      console.log('hi')
-      console.log(level)
+      setLevel(level + 0.25);
+      console.log("hi");
+      console.log(level);
     } else {
       setWater(water + 1);
     }
@@ -23,19 +26,23 @@ function App() {
 
   return (
     <div className="App">
-      <Levels level={level} arg = "hello"/>
-      <img src={plant} className="imgprop"/>
+      <Levels level={level} arg="hello" />
+      <div>
+        <LoginButton />
+        <LogoutButton />
+        <Profile/>
+      </div>
+      <img src={plant} className="imgprop" />
 
       <div className="water">
         <div className="progBar">
           <ProgressBar animated variant="success" now={water} max={21} />
         </div>
         <button onClick={clickHandlerWater} className="waterButton">
-          <img className ="waterlogo" src={waterplant}></img>
+          <img className="waterlogo" src={waterplant}></img>
         </button>
       </div>
       {/* <h1>{water}</h1> */}
-
     </div>
   );
 }
