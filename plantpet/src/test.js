cece3ae6933dd73d089ede134c97448bfd1cf875
Dohyun Lee <leedohyun80@gmail.com>
2021-11-08 09:41:55 -0500

    not quite sure how to set up ajax req

--- plantpet/src/test.js
+++ plantpet/src/test.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 4000
+const fetch = require('node-fetch')
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -10,3 +11,8 @@ app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
 
+fetch(//notsure what goes in here)
+  .then(response => response.json())
+  .then(data => {
+    console.log(data)
+  })
