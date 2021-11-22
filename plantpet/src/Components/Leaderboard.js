6130f6e56fff0e54e183bb706f5516d484919426
aschrad1 <aschrad1@swarthmore.edu>
2021-11-21 23:31:34 -0500

    sorting leaderboard

--- plantpet/src/Components/Leaderboard.js
+++ plantpet/src/Components/Leaderboard.js
@@ -6,7 +6,7 @@ const Leaderboard = () => {
   const [allData, setAllData] = useState([]);
   const axios = require("axios");
   axios.get("http://localhost:2500/users").then(function (response) {
-    setAllData(response.data["users"].map((x) => <p>{x.Name}: {Math.round(x.PlantLevel)}</p>));
+    setAllData(response.data["users"].sort((a,b) => b.PlantLevel-a.PlantLevel).map((x) => <p>{x.Name}: {Math.round(x.PlantLevel)}</p>));
     console.log("here--");
     console.log(allData);
     console.log("here--");
