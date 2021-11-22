f2f61eacb6e54b4e1d83a888231b4056bc591824
aschrad1 <aschrad1@swarthmore.edu>
2021-11-22 15:39:05 -0500

    background changes

--- plantpet/src/Components/Seed.js
+++ plantpet/src/Components/Seed.js
@@ -2,11 +2,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 // import { useState } from "react";
 import "./Seed.css";
 
-<<<<<<< HEAD
-const Seed = () => {
-=======
-const Seed = (props) => {
->>>>>>> ee8a15a2e92e31f67c7bb7727767f25fdb87436f
+const Seed =   (props) => {
   return (
     <div className="seed">
       <b>Seeds:</b> {props.seed}
