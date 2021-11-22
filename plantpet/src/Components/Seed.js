9d2e893e4a9210916d67891fc7dfb4fbcd0bd59a
Catherine Atalig <catherineatalignyc@gmail.com>
2021-11-22 15:24:47 -0500

    merge resolved

--- plantpet/src/Components/Seed.js
+++ plantpet/src/Components/Seed.js
@@ -1,11 +1,9 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 // import { useState } from "react";
 
-<<<<<<< HEAD
-const Seed = () => {
-=======
+
 const Seed = (props) => {
->>>>>>> ee8a15a2e92e31f67c7bb7727767f25fdb87436f
+
   return (
     <div className="seed">
       <b>Seeds:</b> {props.seed}
