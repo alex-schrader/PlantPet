80e78c5a40f1d0514f9dd5989b33bc11736d9ea6
aschrad1 <aschrad1@swarthmore.edu>
2021-11-21 22:36:45 -0500

    deleting users w no name

--- plantpet/src/Components/Leaderboard.js
+++ plantpet/src/Components/Leaderboard.js
@@ -6,7 +6,7 @@ const Leaderboard = () => {
   const [allData, setAllData] = useState([]);
   const axios = require("axios");
   axios.get("http://localhost:2500/users").then(function (response) {
-    setAllData(response.data["users"].map((x) => <p>{x.Name}: {x.PlantLevel}</p>));
+    setAllData(response.data["users"].map((x) => <p>{x.Name}: {Math.round(x.PlantLevel)}</p>));
     console.log("here--");
     console.log(allData);
     console.log("here--");
