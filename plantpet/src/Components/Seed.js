1e0e3cd6045595586f36530c33158b05eb1db478
aschrad1 <aschrad1@swarthmore.edu>
2021-11-24 23:25:06 -0500

    adding font to seeds

--- plantpet/src/Components/Seed.js
+++ plantpet/src/Components/Seed.js
@@ -1,13 +1,11 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 // import { useState } from "react";
-import "./Seed.css"
-
+import "./Seed.css";
 
 const Seed = (props) => {
-
   return (
-    <p className = "seed">
-      Seeds: {props.seed}
+    <p className="seed">
+        Seeds: {props.seed}‎‎‎‎
     </p>
   );
 };
