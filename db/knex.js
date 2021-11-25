990fdd4a57b50e75892f8a6a816c01a9f3ec368b
Dohyun Lee <leedohyun80@gmail.com>
2021-11-24 19:17:52 -0500

    db update

--- db/knex.js
+++ db/knex.js
@@ -1,5 +1,5 @@
 const knex = require("knex");
-
+//connect sqlite3 to database for storing user data and progress
 const connectedKnex = knex({
     client: "sqlite3",
     connection: {
--- db/users.js
+++ db/users.js
@@ -1,17 +1,22 @@
 const knex = require("./knex");
 
+//create a new user
 function createUser(user) {
   return knex("users").insert(user);
 }
 
+//retrieve all user data
 function getAllUsers() {
   return knex("users").select("*");
 }
 
+
+//delete a user
 function deleteUser(UserID) {
   return knex("users").where("UserID", UserID).del();
 }
 
+//delete user information
 function updateUser(UserID, user) {
   return knex("users").where("UserID", UserID).update(user);
 }
